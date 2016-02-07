dc778e5b66adcbb87176aa2db306ff4ecaa233cc
Hyunsik, Kim(Hoya) <wbstory@storymate.net>
2016-02-07 11:06:39 +0900

    show description in style.

--- src/components/SubmissionItem.jsx
+++ src/components/SubmissionItem.jsx
@@ -1,16 +1,50 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, Col, Panel, Table, Button, Popover, Overlay, OverlayTrigger, Modal } from 'react-bootstrap';
+import { Grid, Col, Panel, Table, ButtonGroup, Button, Popover, Overlay, OverlayTrigger, Modal } from 'react-bootstrap';
 
+import { ib_query } from './utils';
 
 class Picture extends React.Component {
   render() {
     return(
-      <img className="img-responsive" src={this.props.src} />
+      <img style={this.props.style} className="img-responsive" src={this.props.src} />
     )
   }
+}
 
+export class Description extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: "",
+    }
+  }
+
+  componentWillMount() {
+    const sid = window.localStorage.getItem('sid');
+    const submission_id = this.props.submission_id;
+
+    ib_query(
+      'https://inkbunny.net/api_submissions.php',
+      //{sid:this.state.sid, username:username, page:1, get_rid:'yes'},
+      {sid:sid, submission_ids:submission_id, show_description:'yes'},
+      (data) => {
+        this.setState({
+          description: data.submissions[0].description,
+        });
+      }
+    );
+  }
+
+  render() {
+    return(
+      <div style={{color:'black'}}>
+        {this.state.description}
+      </div>
+    );
+  }
 }
 
 export class SubmissionItem extends React.Component {
@@ -20,11 +54,16 @@ export class SubmissionItem extends React.Component {
     this.state = {
       showModal: false,
       showPopover: false,
+      opacity: 1,
     }
   }
 
-  componentDidMount() {
-
+  toggleDescription() {
+    if(this.state.opacity == 1) {
+      this.setState({opacity:0.2});
+    } else {
+      this.setState({opacity:1});
+    }
   }
 
   render() {
@@ -47,12 +86,27 @@ export class SubmissionItem extends React.Component {
                bsSize="large"
                onHide={e => this.setState({showModal:false})}>
           <Modal.Header closeButton>
-            <Modal.Title>{this.props.item.title}</Modal.Title>
+            <Modal.Title>
+              <Grid fluid={true}>
+                <Col xs={8}>
+                  <span>{this.props.item.title}</span>
+                </Col>
+                <Col xs={4}>
+                  <ButtonGroup>
+                    <Button bsStyle='default' onClick={this.toggleDescription.bind(this)} ><i className="fa fa-info" /></Button>
+                    <a className="btn btn-default" href={this.props.item.file_url_full} target="_blank">
+                      <i className="fa fa-expand" />
+                    </a>
+                  </ButtonGroup>
+                </Col>
+              </Grid>
+            </Modal.Title>
           </Modal.Header>
-          <Modal.Body>
-            <a href={this.props.item.file_url_full} target="_blank">
-              <Picture src={this.props.item.file_url_full} />
-            </a>
+          <Modal.Body style={{position:'relative'}}>
+            <Picture style={{'opacity':this.state.opacity}} src={this.props.item.file_url_full} />
+            <div style={{position:'absolute', top:"0", opacity:1-this.state.opacity}}>
+              <Description submission_id={this.props.item.submission_id} />
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <p>by {this.props.item.username}</p>
